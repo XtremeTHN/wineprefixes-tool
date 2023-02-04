7d700be04ad2693d2004214f77f6a9e47c5798e7
XtremeTHN <nigthmaresans2@gmail.com>
2023-02-04 14:04:39 -0600

    Funciones extra

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ import argparse, os, glob, sys
 parser = argparse.ArgumentParser("wineprefixes tool", "wineprefixes 0.0", "Herramienta para manipular directorios de windows emulados (wineprefixes)")
 
 parser.add_argument("-g", "--get", action="store", dest="get", metavar="PREFIX")
+parser.add_argument("-l", "--list", action="store_true", dest="ls")
+parser.add_argument("-gp", "--get-path", action="store_true", dest="gpath")
+parser.add_argument("-sp", "--set-path", action="store", dest="path", metavar="path")
 parser.add_argument("-rm", "--remove", action="store", dest="rm", metavar="PREFIX")
 
 obj = parser.parse_args()
 home = os.getenv("HOME")
 path_prefix = os.path.join(os.getenv("HOME"), '.local', 'share', 'wineprefixes')
+if obj.path != None:
+    path_prefix = obj.path
+
+if not os.path.exists(path_prefix):
+    os.system(f"mkdir -p {path_prefix}")
+
+if obj.gpath:
+    print(path_prefix)
+    sys.exit(0)
+
+if obj.ls:
+    print(glob.glob(path_prefix + '/*'))
+    sys.exit(0)
 
 if obj.get != None:
     prefixes = glob.glob(path_prefix + "/*")
